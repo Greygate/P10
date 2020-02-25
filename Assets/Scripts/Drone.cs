3f4db075221c1fa2a38c68ab929b4cb586a93a7f
Matias <sejemus@yahoo.dk>
2020-02-25 15:42:28 +0100

    Added Vicon plugin

---
+++ Assets/Scripts/Drone.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drone : MonoBehaviour
+{
+    Vector3 startPosition;
+
+    bool alerted = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (alerted)
+        {
+            // Make path from start position to current position
+        }
+    }
+}
---
+++ Assets/Scripts/ViconManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityVicon;
+
+[RequireComponent(typeof(ViconDataStreamClient))]
+public class ViconManager : MonoBehaviour
+{
+    public int numDrones = 1;
+
+    public GameObject dronePrefab;
+
+    ViconDataStreamClient client;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        client = GetComponent<ViconDataStreamClient>();
+
+        for (int i = 0; i < numDrones; i++)
+        {
+            GameObject drone = Instantiate(dronePrefab);
+            RBScript script = drone.AddComponent<RBScript>();
+            script.Client = client;
+            script.ObjectName = $"Drone_{i}";
+        }
+    }
+}
